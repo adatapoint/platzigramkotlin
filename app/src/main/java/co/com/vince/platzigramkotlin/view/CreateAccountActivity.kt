b4b67e83037cf457fea38bb2beaac32b838dfd34
Vincent Restrepo <vdrestrepo@geosat.com.co>
2019-03-22 14:10:45 -0500

    A good login interface

---
+++ app/src/main/java/co/com/vince/platzigramkotlin/view/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package co.com.vince.platzigramkotlin.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import co.com.vince.platzigramkotlin.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
