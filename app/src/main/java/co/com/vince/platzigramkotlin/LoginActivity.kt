705722f3174b83372572cbc0f1f73c873e9f7052
Vincent Restrepo <vdrestrepo@geosat.com.co>
2019-03-27 14:00:00 -0500

    Done with login and create Account

--- app/src/main/java/co/com/vince/platzigramkotlin/LoginActivity.kt
+++ app/src/main/java/co/com/vince/platzigramkotlin/LoginActivity.kt
@@ -1,14 +1,23 @@
 package co.com.vince.platzigramkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import co.com.vince.platzigramkotlin.view.CreateAccountActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
 
+    fun goCreateAccount(v: View) {
+        var intent: Intent = Intent(baseContext, CreateAccountActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/co/com/vince/platzigramkotlin/view/CreateAccountActivity.kt
+++ app/src/main/java/co/com/vince/platzigramkotlin/view/CreateAccountActivity.kt
@@ -3,11 +3,22 @@ package co.com.vince.platzigramkotlin.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.com.vince.platzigramkotlin.R
+import kotlinx.android.synthetic.main.actionbar_toolbar.*
 
 class CreateAccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        toolbarConfig(resources.getString(R.string.create_account), true)
+
+
     }
+
+    private fun toolbarConfig(title: String, upButton: Boolean) {
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = title
+        supportActionBar!!.setDisplayHomeAsUpEnabled(upButton)
+    }
+
 }
